245bdf056b3365d24bb493de5421f903a8c014ca
Bernhard Kerbl <kerbl@icg.tugraz.at>
2021-05-02 14:24:29 +0200

    Added code comments

--- 16_CooperativeGroups/src/main.cu
+++ 16_CooperativeGroups/src/main.cu
@@ -45,7 +45,7 @@ void ReduceWithGroups()
 {
     constexpr unsigned int BLOCK_SIZE = 256, N = 1'000'000;
 
-    std::cout << "Producing random inputs...\n" << std::endl;
+    std::cout << "Producing random inputs..." << std::endl;
     // Generate some random numbers to reduce
     std::vector<float> vals;
     float* dValsPtr;
@@ -69,49 +69,59 @@ void ReduceWithGroups()
     // Synchronizing to event. Event is last, same effect as cudaDeviceSynchronize
     cudaEventSynchronize(end);
     cudaEventElapsedTime(&ms, start, end);
-    std::cout << std::setw(20) << "Reduce Groups" << "\t" << ms << "ms \t" << mResult << std::endl;
+    std::cout << std::setw(20) << "Reduce Groups" << "\t" << ms << "ms \t" << mResult << "\n\n";
 }
 
 __managed__ unsigned int mHappyNumSum;
 __managed__ unsigned int mHappyNumCount;
 
-__global__ void Sum10HappyNumbers(unsigned int N, unsigned int* mHappyNumbers)
+__global__ void ComputeHappyNumbers(unsigned int start, unsigned int N, unsigned int* mHappyNumbers)
 {
-    unsigned int input = cg::this_grid().thread_rank() + 1;
-
+    // Retrieve the input number based on the thread's global id
+    unsigned int input = cg::this_grid().thread_rank() + start;
+    // Compute whether or not a number is "happy" (utility function)
     bool happy = (input <= N) && samplesutil::isHappy(input);
-
+    // Create a group for the current warp
     auto warp = cg::tiled_partition<32>(cg::this_thread_block());
+    // Create a child group to separate threads with happy and unhappy numbers
     auto g = cg::binary_partition(warp, happy);
 
     if (happy)
     {
-        unsigned int offset;
+        // Compute the group's partial result of the sum of happy numbers
         unsigned int partial_sum = cg::reduce(g, input, cg::plus<unsigned int>());
+        // One thread uses aggregate atomics to compute sum and write offset of happy numbers
+        unsigned int offset;
         if (g.thread_rank() == 0)
         {
             atomicAdd(&mHappyNumSum, partial_sum);
             offset = atomicAdd(&mHappyNumCount, g.size());
         }
+        // Distribute offset information from the thread that computed it to all others
         offset = g.shfl(offset, 0);
+        // Each thread writes their happy number in a unique location
         mHappyNumbers[offset + g.thread_rank()] = input;
     }
 }
 
-void HappyNummbersWithGroups(unsigned int N)
+void HappyNummbersWithGroups(unsigned int start, unsigned int N)
 {
+    // Initialize / allocate result storage. For brevity, we use managed memory
     mHappyNumSum = 0;
     mHappyNumCount = 0;
     unsigned int* mHappyNumbers;
     cudaMallocManaged((void**)&mHappyNumbers, sizeof(unsigned int) * N);
 
-    Sum10HappyNumbers<<<(N + 255) / 256, 256>>>(N, mHappyNumbers);
+    // Compute count, sum and list of base 10 "happy numbers" from start to start+N
+    ComputeHappyNumbers<<<(N + 255) / 256, 256>>>(start, N, mHappyNumbers);
     cudaDeviceSynchronize();
 
-    std::cout << "No. of happy numbers from 1 - " << N << ": " << mHappyNumCount << std::endl;
-    std::cout << "Sum of happy numbers from 1 - " << N << ": " << mHappyNumSum << std::endl;
-    std::cout << "\nList of happy numbers from 1 - " << N << ": ";
+    // Print the count, sum and list of happy numbers in the given range
+    std::cout << "No. of happy numbers in " << start << " - " << N << ": " << mHappyNumCount << "\n";
+    std::cout << "Sum of happy numbers in " << start << " - " << N << ": " << mHappyNumSum << "\n";
+    std::cout << "\nList of happy numbers in " << start << " - " << N << ": ";
 
+    // Sort the managed memory happy number list in ascending order
     std::sort(mHappyNumbers, mHappyNumbers + mHappyNumCount);
     for (int i = 0; i < mHappyNumCount; i++)
         std::cout << mHappyNumbers[i] << ((i == mHappyNumCount - 1) ? "\n" : ", ");
@@ -126,17 +136,22 @@ int main()
     essence, they package a range of recent CUDA features in an interface
     that abstracts away the low-level instructions, making CUDA code 
     easier to understand. As such, cooperative groups have a vast range
-    of applications. The examples in this project cannot do them justice,
-    for further use cases please consider the advanced NVIDIA Samples 
-    that include detailed, elaborate applications.
+    of applications. The two examples in this project cannot do them 
+    justice, for further use cases please consider the advanced NVIDIA 
+    CUDA Samples that include detailed, elaborate applications.
 
     Expected output:
     1) Result of reduction, now computed with cooperative groups
+    2) The count, sum and list of the happy numbers in a given range (1-1000)
     */
 
-    //ReduceWithGroups();
+    std::cout << "==== Computing a Reduction with Cooperative Groups ====" << std::endl;
 
-    HappyNummbersWithGroups(1000);
+    ReduceWithGroups();
+
+    std::cout << "==== Computing Happy Numbers and their Sum ====" << std::endl;
+
+    HappyNummbersWithGroups(1, 1000);
     
     return 0;
 }
--- shared/include/utility.h
+++ shared/include/utility.h
@@ -7,81 +7,90 @@
 #include <vector>
 #include <cuda_runtime_api.h>
 
+#ifndef SAMPLES_UTIL_INCLUDED
+#define SAMPLES_UTIL_INCLUDED
+
 namespace samplesutil
 {
-__host__ void prepareRandomNumbersCPUGPU(unsigned int N, std::vector<float>& vals, float** dValsPtr)
-{
-	constexpr float target = 42.f;
-	// Print expected value, because reference may be off due to floating point (im-)precision
-	std::cout << "\nExpected value: " << target * N << "\n" << std::endl;
-
-	// Generate a few random inputs to accumulate
-	std::default_random_engine eng(0xcaffe);
-	std::normal_distribution<float> dist(target);
-	vals.resize(N);
-	std::for_each(vals.begin(), vals.end(), [&dist, &eng](float& f) { f = dist(eng); });
-
-	// Allocate some global GPU memory to write the inputs to
-	cudaMalloc((void**)dValsPtr, sizeof(float) * N);
-	// Expliclity copy the inputs from the CPU to the GPU
-	cudaMemcpy(*dValsPtr, vals.data(), sizeof(float) * N, cudaMemcpyHostToDevice);
-}
-
-// Define an unsigned integer variable that the GPU can work with
-__device__ unsigned int step = 0;
-
-// Increment the GPU variable N times. Whenever a thread observes
-// non-consecutive numbers, it prints the latest sequence. Hence,
-// every thread documents the turns that it was given by the 
-// scheduler. 
-__device__ void takeNTurns(const char* who, unsigned int N)
-{
-    unsigned int lastTurn = 0, turn, start;
-    for (int i = 0; i < N; i++)
+    static __host__ void prepareRandomNumbersCPUGPU(unsigned int N, std::vector<float>& vals, float** dValsPtr)
     {
-        turn = atomicInc(&step, 0xFFFFFFFFU);
-        if (lastTurn != (turn-1))
-            start = turn;
+	    constexpr float target = 42.f;
+	    // Print expected value, because reference may be off due to floating point (im-)precision
+	    std::cout << "\nExpected value: " << target * N << "\n" << std::endl;
 
-        if ((i == N - 1) || ((i > 0) && (start == turn)))
-            printf("%s: %d--%d\n", who, start, turn);
+	    // Generate a few random inputs to accumulate
+	    std::default_random_engine eng(0xcaffe);
+	    std::normal_distribution<float> dist(target);
+	    vals.resize(N);
+	    std::for_each(vals.begin(), vals.end(), [&dist, &eng](float& f) { f = dist(eng); });
 
-        lastTurn = turn;
+	    // Allocate some global GPU memory to write the inputs to
+	    cudaMalloc((void**)dValsPtr, sizeof(float) * N);
+	    // Expliclity copy the inputs from the CPU to the GPU
+	    cudaMemcpy(*dValsPtr, vals.data(), sizeof(float) * N, cudaMemcpyHostToDevice);
     }
-}
 
-__global__ void testScheduling(int N)
-{
-    if (threadIdx.x < 2) // Branch once
-        if (threadIdx.x == 0) // Branch again
-            takeNTurns("Thread 1", N);
-        else
-            takeNTurns("Thread 2", N);
-    else
-        if (threadIdx.x == 2) // Branch again
-            takeNTurns("Thread 3", N);
-        else
-            takeNTurns("Thread 4", N);
-}
+    // Define an unsigned integer variable that the GPU can work with
+    __device__ unsigned int step = 0;
 
-__host__ void run2NestedBranchesForNSteps(int N)
-{
-	testScheduling<<<1, 4>>>(N);
-	cudaDeviceSynchronize();
-}
-
-__host__ __device__ bool isHappy(unsigned int num)
-{
-    while (num != 0 && num != 1 && num != 4)
+    // Increment the GPU variable N times. Whenever a thread observes
+    // non-consecutive numbers, it prints the latest sequence. Hence,
+    // every thread documents the turns that it was given by the 
+    // scheduler. 
+    static __device__ void takeNTurns(const char* who, unsigned int N)
     {
-        unsigned int next_num = 0;
-        for (unsigned int n = num; n > 0; n /= 10)
+        unsigned int lastTurn = 0, turn, start;
+        for (int i = 0; i < N; i++)
         {
-            unsigned int t = n % 10;
-            next_num += t * t;
+            turn = atomicInc(&step, 0xFFFFFFFFU);
+            if (lastTurn != (turn-1))
+                start = turn;
+
+            if ((i == N - 1) || ((i > 0) && (start == turn)))
+                printf("%s: %d--%d\n", who, start, turn);
+
+            lastTurn = turn;
         }
-        num = next_num;
     }
-    return num == 1;
+
+    static __global__ void testScheduling(int N)
+    {
+        if (threadIdx.x < 2) // Branch once
+            if (threadIdx.x == 0) // Branch again
+                takeNTurns("Thread 1", N);
+            else
+                takeNTurns("Thread 2", N);
+        else
+            if (threadIdx.x == 2) // Branch again
+                takeNTurns("Thread 3", N);
+            else
+                takeNTurns("Thread 4", N);
+    }
+
+    static __host__ void run2NestedBranchesForNSteps(int N)
+    {
+	    testScheduling<<<1, 4>>>(N);
+	    cudaDeviceSynchronize();
+    }
+
+    /*
+     Computes whether a given number is a "happy number".
+     https://en.wikipedia.org/wiki/Happy_number
+    */
+    static __host__ __device__ bool isHappy(unsigned int num)
+    {
+        while (num != 0 && num != 1 && num != 4)
+        {
+            unsigned int next_num = 0;
+            for (unsigned int n = num; n > 0; n /= 10)
+            {
+                unsigned int t = n % 10;
+                next_num += t * t;
+            }
+            num = next_num;
+        }
+        return num == 1;
+    }
 }
-}
+
+#endif
