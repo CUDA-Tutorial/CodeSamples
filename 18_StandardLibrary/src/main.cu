8234233d763a8b8d29ea8996d7946e894501c97d
Bernhard Kerbl <kerbl@icg.tugraz.at>
2021-11-29 22:26:49 +0100

    Improved std lib atomic example

--- 18_StandardLibrary/src/main.cu
+++ 18_StandardLibrary/src/main.cu
@@ -6,6 +6,8 @@
 #include <algorithm>
 #include <cuda/std/atomic>
 
+__managed__ cuda::std::atomic<int> mResultCPU{ 0 }, mResultGPU{ 0 };
+
 /*
  Basic, architecture-agnostic reduction, using global atomics.
  Uses portable cuda::std::atomics. Note that even though this code is portable,
@@ -14,7 +16,7 @@
  performance may be achieved using cuda::atomics, which take an additional parameter
  "thread_scope" (e.g., "device" for global, "block" for shared memory atomics).
 */
-__host__ __device__ void reduceAtomic(int tId, int numThreads, int N, const int* input, cuda::std::atomic<int>* result)
+__host__ __device__ void reduceAtomic(int tId, int numThreads, int N, const int* input, cuda::std::atomic<int>& result)
 {
 	if (tId >= N)
 		return;
@@ -26,21 +28,21 @@ __host__ __device__ void reduceAtomic(int tId, int numThreads, int N, const int*
 
 	// For each value in the assigned portion, atomically add it to accumulated sum.
 	for (int i = myStart; i < myEnd; i++)
-		result->fetch_add(input[i], cuda::std::memory_order_relaxed);
+		result.fetch_add(input[i], cuda::std::memory_order_relaxed);
 }
 
-__global__ void completeReductionGPU(int N, const int* input, cuda::std::atomic<int>* result)
+__global__ void completeReductionGPU(int N, const int* input)
 {
 	// Launchpad code for calling the architecutre-agnostic reduction function
 	int tId = blockIdx.x * blockDim.x + threadIdx.x;
-	reduceAtomic(tId, N, N, input, result);
+	reduceAtomic(tId, N, N, input, mResultGPU);
 }
 
 template<unsigned int NUM_THREADS>
-__host__ void completeReductionCPU(int N, int* mNumbers, cuda::std::atomic<int>* result)
+__host__ void completeReductionCPU(int N, int* mNumbers)
 {
 	/*
-	 Simple multi-threaded launch for computing the reduction 
+	 Simple multi-threaded launch for computing the reduction
 	 in parallel. This function also makes sure to join on the
 	 threads. Otherwise, we would have to be careful not to
 	 overlap CPU access to managed memory and GPU execution.
@@ -49,7 +51,7 @@ __host__ void completeReductionCPU(int N, int* mNumbers, cuda::std::atomic<int>*
 	*/
 	std::vector<std::thread> threads(NUM_THREADS);
 	for (int i = 0; i < threads.size(); i++)
-		threads[i] = std::thread(reduceAtomic, i, NUM_THREADS, N, mNumbers, result);
+		threads[i] = std::thread(reduceAtomic, i, NUM_THREADS, N, mNumbers, std::ref(mResultCPU));
 	for (std::thread& t : threads)
 		t.join();
 }
@@ -58,23 +60,23 @@ int main()
 {
 	std::cout << "==== Sample 18 - Standard Library ====\n" << std::endl;
 	/*
-	The libcu++ standard library allows us to make code more portable. 
+	The libcu++ standard library allows us to make code more portable.
 	Users can use familiar concepts from programming for the CPU and
-	apply them with minimal changes on the GPU as well. In this 
+	apply them with minimal changes on the GPU as well. In this
 	example, we show a method for parallel reduction where the same
 	reduction function can be executed by a thread on the CPU or the
 	GPU thanks to the support for std:: atomics.
 
 	We use integers in this example. Float atomics are only part
-	of the standard in C++20. Should be widely available soon! Once 
-	more, the libcu++ standard library is much more powerful than 
+	of the standard in C++20. Should be widely available soon! Once
+	more, the libcu++ standard library is much more powerful than
 	we can show with just a few samples. We encourage you to check
 	out the documentation for libcu++, as well as related talks for
-	more examples of use cases. 
+	more examples of use cases.
 
 	Expected output: the result of a reduction with random integers,
-	once computed on the CPU and once on the GPU, both of them 
-	yielding the same accumulated value. 
+	once computed on the CPU and once on the GPU, both of them
+	yielding the same accumulated value.
 	*/
 
 	// Define the number of inputs to reduce, and number of CPU threads
@@ -86,24 +88,19 @@ int main()
 	std::uniform_int_distribution<int> dist(10, 42);
 	cudaMallocManaged((void**)&mNumbers, sizeof(int) * N);
 	std::for_each(mNumbers, mNumbers + N, [&dist, &eng](int& v) { v = dist(eng); });
-	// Allocate managed memory for the computed recution results (CPU/GPU)
-	cuda::std::atomic<int>* mResults;
-	cudaMallocManaged((void**)&mResults, 2 * sizeof(cuda::std::atomic<int>));
-	mResults[0] = mResults[1] = 0;
 
 	// Launch the reduction with the given number of CPU threads
-	completeReductionCPU<CPUThreads>(N, mNumbers, &mResults[0]);
+	completeReductionCPU<CPUThreads>(N, mNumbers);
 	// Launch the reduction on the GPU with as many threads as there are inputs
-	completeReductionGPU<<<(N + 255) / 256, 256>>>(N, mNumbers, &mResults[1]);
+	completeReductionGPU << <(N + 255) / 256, 256 >> > (N, mNumbers);
 	cudaDeviceSynchronize();
 
 	// Output both results
-	std::cout << "Reduction result CPU: " << mResults[0] << "\n" << std::endl;
-	std::cout << "Reduction result GPU: " << mResults[1] << "\n" << std::endl;
+	std::cout << "Reduction result CPU: " << mResultCPU << "\n" << std::endl;
+	std::cout << "Reduction result GPU: " << mResultGPU << "\n" << std::endl;
 
 	// finally, release the managed memory for the inputs and results
 	cudaFree(mNumbers);
-	cudaFree(mResults);
 }
 
 /*
@@ -115,5 +112,5 @@ and demonstrate it.
 single cuda::atomic variable. For performance reasons, it should be one with
 thread_scope "block".
 3) Try to show that there is a performance difference in 2) between using
-the default cuda::std::atomic and the cuda::atomic with thread_scope block.
+the default cuda::std::atomic and the cuda::atomic with block thread_scope.
 */
